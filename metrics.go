a5bd8d604e3e8954952001c24f83b2d5f89557df
zhiqiangxu <652732310@qq.com>
2019-11-15 12:33:05 +0800

    add metrics.go

---
+++ metrics.go
@@ -0,0 +1,111 @@
+package util
+
+import (
+	"sync"
+
+	kitmetrics "github.com/go-kit/kit/metrics"
+
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+
+	// metric maps
+
+	counterMap = make(map[string]kitmetrics.Counter)
+	cl         sync.Mutex
+
+	gaugeMap = make(map[string]kitmetrics.Gauge)
+	gl       sync.Mutex
+
+	histMap = make(map[string]kitmetrics.Histogram)
+	hl      sync.Mutex
+)
+
+// RegisterCounter registers a counter
+func RegisterCounter(name string, labels []string) kitmetrics.Counter {
+	cl.Lock()
+	defer cl.Unlock()
+
+	counter, ok := counterMap[name]
+	if ok {
+		panic("registered the same counter twice")
+	}
+
+	counter = kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{Name: name}, labels)
+	counterMap[name] = counter
+	return counter
+}
+
+// GetCounter get a Counter by name
+func GetCounter(name string) kitmetrics.Counter {
+	cl.Lock()
+	defer cl.Unlock()
+
+	counter, ok := counterMap[name]
+	if ok {
+		return counter
+	}
+
+	panic("unregistered counter")
+
+}
+
+// RegisterGauge registers a gauge
+func RegisterGauge(name string, labels []string) kitmetrics.Gauge {
+	gl.Lock()
+	defer gl.Unlock()
+
+	gauge, ok := gaugeMap[name]
+	if ok {
+		panic("registered the same gauge twice")
+	}
+
+	gauge = kitprometheus.NewGaugeFrom(stdprometheus.GaugeOpts{Name: name}, labels)
+	gaugeMap[name] = gauge
+	return gauge
+}
+
+// GetGauge get a Gauge by name
+func GetGauge(name string) kitmetrics.Gauge {
+	gl.Lock()
+	defer gl.Unlock()
+
+	gauge, ok := gaugeMap[name]
+	if ok {
+		return gauge
+	}
+
+	panic("unregistered gauge")
+
+}
+
+// RegisterHist registers a hist
+func RegisterHist(name string, labels []string) kitmetrics.Histogram {
+	hl.Lock()
+	defer hl.Unlock()
+
+	hist, ok := histMap[name]
+	if ok {
+		panic("registered the same gauge twice")
+	}
+
+	hist = kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{Name: name}, labels)
+	histMap[name] = hist
+	return hist
+}
+
+// GetHist get a Hist by name
+func GetHist(name string) kitmetrics.Histogram {
+	hl.Lock()
+	defer hl.Unlock()
+
+	hist, ok := histMap[name]
+	if ok {
+		return hist
+	}
+
+	panic("unregistered gauge")
+
+}
