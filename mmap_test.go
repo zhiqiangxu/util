05e232e5997884fa2142b49d8e0876ffc0f3f9d3
zhiqiangxu <652732310@qq.com>
2019-11-24 10:21:45 +0800

    update mmap_test.go

--- mmap_test.go
+++ mmap_test.go
@@ -9,6 +9,7 @@ import (
 
 func TestMmap(t *testing.T) {
 	fileName := "/tmp/test_util.txt"
+	os.Remove(fileName)
 	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
 	assert.Assert(t, err == nil)
 	defer func() {
@@ -17,8 +18,11 @@ func TestMmap(t *testing.T) {
 	}()
 
 	size := 64000
-	bytes, err := Mmap(f, false, int64(size))
+	n, err := f.Write(make([]byte, size))
+	assert.Assert(t, n == size && err == nil)
+	bytes, err := Mmap(f, true, int64(size))
 	assert.Assert(t, err == nil && len(bytes) == size)
+	bytes[0] = 0
 	err = Madvise(bytes, true)
 	assert.Assert(t, err == nil)
 	err = Madvise(bytes, false)
