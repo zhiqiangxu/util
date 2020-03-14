61e6c9eb62eae1c33f6624a83befc0e89e7ef6fe
徐志强 <652732310@qq.com>
2020-03-14 11:02:58 +0800

    add TestSemaphoreBug

--- mutex/crwmutex_test.go
+++ mutex/crwmutex_test.go
@@ -8,6 +8,7 @@ import (
 
 func TestCRWMutex(t *testing.T) {
 	crwm := NewCRWMutex()
+	// test RLock/RUnlock
 	err := crwm.RLock(context.Background())
 	if err != nil {
 		t.FailNow()
@@ -15,12 +16,14 @@ func TestCRWMutex(t *testing.T) {
 
 	crwm.RUnlock()
 
+	// test Lock/Unlock
 	err = crwm.Lock(context.Background())
 	if err != nil {
 		t.FailNow()
 	}
 	crwm.Unlock()
 
+	// test RLock/canceled Lock/RUnlock
 	err = crwm.RLock(context.Background())
 	if err != nil {
 		t.FailNow()
@@ -34,5 +37,40 @@ func TestCRWMutex(t *testing.T) {
 	}
 
 	crwm.RUnlock()
+
+}
+
+func TestSemaphoreBug(t *testing.T) {
+	crwm := NewCRWMutex()
+	crwm.RLock(context.Background())
+	crwm.RLock(context.Background())
+
+	go func() {
+		ctx, cancelFunc := context.WithTimeout(context.Background(), time.Millisecond*300)
+		defer cancelFunc()
+		err := crwm.Lock(ctx)
+		if err == nil {
+			t.FailNow()
+		}
+	}()
+
+	time.Sleep(time.Millisecond * 100)
+	doneCh := make(chan struct{})
+	go func() {
+		err := crwm.RLock(context.Background())
+		if err != nil {
+			t.FailNow()
+		}
+		crwm.RUnlock()
+		close(doneCh)
+	}()
+
+	crwm.RUnlock()
+
+	select {
+	case <-doneCh:
+	case <-time.After(time.Second):
+		t.FailNow()
+	}
 
 }
