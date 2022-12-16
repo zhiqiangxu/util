d6d08bd3efc378bd6c1235de077d832633f3743f
zhiqiangxu <652732310@qq.com>
2022-12-17 03:02:25 +0800

    add math.R2

--- math/pi.go
+++ math/pi.go
@@ -27,3 +27,39 @@ func Pi(n int) int {
 	return count
 
 }
+
+// R2 returns # of ordered prime pairs with sum n
+func R2(n int) int {
+	if n <= 1 {
+		return 0
+	}
+
+	// store numbers in range [2,n]
+	numbers := make([]int, n-1)
+	for i := range numbers {
+		numbers[i] = i + 2
+	}
+
+	// sieve method
+	for i, v := range numbers {
+		if v == i+2 {
+			// v is a prime
+			for j := i + v; j <= n-2; j += v {
+				numbers[j] = 0
+			}
+		}
+	}
+
+	count := 0
+	for i, v := range numbers {
+		if v == i+2 {
+			// v is a prime
+			j := n - v - 2
+			if j >= 0 && numbers[j] > 0 {
+				count++
+				numbers[j] = 0
+			}
+		}
+	}
+	return count
+}
--- math/pi_test.go
+++ math/pi_test.go
@@ -11,3 +11,8 @@ func TestPi(t *testing.T) {
 	assert.Equal(t, Pi(4), 2)
 	assert.Equal(t, Pi(6), 3)
 }
+
+func TestR2(t *testing.T) {
+	assert.Equal(t, R2(5), 1)
+	assert.Equal(t, R2(6), 1)
+}
